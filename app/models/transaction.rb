a6dba94b18c5e02380ee64252f790dfdeb438dfe
Croix Kimmel <croixkimmel@gmail.com>
2022-04-24 16:52:05 -0600

    Add all_balances method, to return summed balances by payer

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
   validates_presence_of :payer, require: true
   validates_presence_of :points, require: true
+
+  def Transaction.all_balances
+    Transaction.group(:payer).sum(:points)
+  end
 end
