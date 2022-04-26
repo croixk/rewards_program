bc99463bca489d78fdcfc4d247ba39ace9bd0198
Croix Kimmel <croixkimmel@gmail.com>
2022-04-25 20:03:55 -0600

    Update test for 200 status

--- spec/requests/api/v1/users/add_transaction_spec.rb
+++ spec/requests/api/v1/users/add_transaction_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Transactions' do
     transaction = Transaction.create(payer: 'DANNON', points: 1000, created_at: "2020-11-02T14:00:00Z", updated_at: "2020-11-02T14:00:00Z")
     post '/api/v1/transactions/add_transaction', params: transaction, as: :json
     expect(response).to be_successful
-    expect(status).to eq(201)
+    expect(status).to eq(200)
   end
 
   it 'does not post if it is missing a field' do
